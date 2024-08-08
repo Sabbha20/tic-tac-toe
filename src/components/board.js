5b56e12c7e47b964a8666f9433e52212000bd1c3
Sabbha20 <sabbha.power1@gmail.com>
2024-08-08 13:54:53 +0530

    lifted state to parents

--- src/components/board.js
+++ src/components/board.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Squares from "./square";
 
 const Board = () => {
+    const initialSquares = [
+        null, null, null,
+        null, null, null,
+        null, null, null,
+    ];
+    const [squares, setSquares] = useState(initialSquares);
+    const handleClickEvent = (i) => {
+        // alert(`box ${i} is clicked!`)
+        //Make a copy of initial state
+        const newSquares = [...squares];
+        //Mutate the copy at i-th position
+        newSquares[i] = "X";
+        //setState to new state
+        setSquares(newSquares);
+    }
 
     const renderSquare = (i) =>{
-        return <Squares />;
+        return (
+            <Squares 
+            value={squares[i]}
+            onClickEvent={()=>handleClickEvent(i)} />
+        );
     }
 
     return <div className="board">
--- src/components/square.js
+++ src/components/square.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 
 const Squares = (props) => {
-    const [value, setValue] = useState(null);
+    // const [value, setValue] = useState(null);
 
-    return <button onClick={() => setValue("X")} className="square">
-        {value}
+    // return <button onClick={() => setValue("X")} className="square">
+    return <button onClick={props.onClickEvent} className="square">
+        {props.value}
     </button>
 }
 
